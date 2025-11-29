48316692306b0da5dabd948cbc3f049b18f6c08d
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-11-29 18:53:27 +0630

    simple code

--- Basic/juliacon.jl
+++ Basic/juliacon.jl
@@ -4,4 +4,11 @@ print("What is your name? ")
 
 name = readline()
 
-println("Hello", name, "!")
+println("Hello ", name, "!")
+
+# age
+
+print("Enter your age: ")
+age_str = readline()
+age = parse(Int, age_str)
+println("You are ", age, " years old.")
